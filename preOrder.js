274861549948e7e7157d449e4b461e02b5ecaecc
Shashank-Girish <96805090+Shashank-Girish@users.noreply.github.com>
2023-02-20 18:01:10 +0530

    preOrderTraversal

---
+++ preOrder.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+function preOrder(root){
+    if (root!==null){
+        console.log(root.data)
+        preOrder(root.left)
+        
+        preOrder(root.right)
+    }
+}
