b02831e308ff1d9e6c20520a31735fa4be0d8544
Shashank-Girish <96805090+Shashank-Girish@users.noreply.github.com>
2023-02-20 17:58:10 +0530

    inOrderTraversal

---
+++ inOrder.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+function inOrder(root){
+    if (root!==null){
+        inOrder(root.left)
+        console.log(root.data)
+        inOrder(root.right)
+    }
+}
