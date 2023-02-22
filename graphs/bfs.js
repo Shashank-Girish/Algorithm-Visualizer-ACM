c20c925cd8f987d7ead8be04146ed708957dfb8a
LohithCV <125025760+LohithCV@users.noreply.github.com>
2023-02-22 18:21:49 +0530

    added searching, bfs, quick and merge sort algos

---
+++ graphs/bfs.js
@@ -0,0 +1,27 @@
+function BFS(graph, node){
+    var visited = new Array(graph.length).fill(false);
+    var queue = [node];
+
+    while(queue.length){
+        var current = queue.shift();
+
+        if(!visited[current]){
+            console.log(current);
+            visited[current] = true;
+
+            for(var i = 0; i < graph.length; i++){
+                if(graph[current][i] && !visited[i]){
+                    queue.push(i);
+                }
+            }
+        }
+    }
+}
+
+// var graph = [ [false, true, true, false, false],
+//           [true, false, true, true, false],
+//           [true, true, false, false, false],
+//           [false, true, false, false, false],
+//           [false, false, false, false, false] ];
+
+// BFS(graph, 4 );
