1463347e5ee68947c6c44c26128c477fbd559304
Shashank-Girish <96805090+Shashank-Girish@users.noreply.github.com>
2023-02-25 15:41:40 +0530

    Animations Working!!!

--- graphs/dfs/dfs.js
+++ graphs/dfs/dfs.js
@@ -1,34 +1,30 @@
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function DFS(graph, node) {
+async function DFS(graph, node) {
   var visited = new Array(graph.length).fill(false);
   var stack = [node];
-  document.getElementById(`white${node}`).style.opacity=0;
-  document.getElementById(`grey${node}`).style.opacity=1;
-  sleep(3000);
+  document.getElementById(`white${node}`).style.display= 'none';
+  document.getElementById(`grey${node}`).style.display='block';
+  await sleep(2000);
   
 
   while (stack.length) {
     var current = stack.pop();
     if (!visited[current]) {
       console.log(current);
-      document.getElementById(`grey${current}`).style.opacity=0;
-      document.getElementById(`black${current}`).style.opacity=1;
-      sleep(3000);
+      document.getElementById(`grey${current}`).style.display='none';
+      document.getElementById(`black${current}`).style.display='block';
+      await sleep(2000);
 
       visited[current] = true;
       for (var i = 0; i < graph.length; i++) {
         if (graph[current][i] && !visited[i]) {
           stack.push(i);
-          document.getElementById(`white${i}`).style.opacity=0;
-          document.getElementById(`grey${i}`).style.opacity=1;
-          sleep(3000);
+          document.getElementById(`white${i}`).style.display='none';
+          document.getElementById(`grey${i}`).style.display='block';
+          await sleep(2000);
         }
       }
     }
