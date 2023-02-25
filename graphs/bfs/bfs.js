21c28ded13a43084ecbfb1c4f9381a87e7c85759
Prerna Lohia <prernalohia@Prernas-MacBook-Air.local>
2023-02-25 16:26:41 +0530

    BFS

--- graphs/bfs/bfs.js
+++ graphs/bfs/bfs.js
@@ -1,18 +1,30 @@
-function BFS(graph, node){
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+async function BFS(graph, node){
     var visited = new Array(graph.length).fill(false);
     var queue = [node];
-    //image- "$node
+    document.getElementById(`white${node}`).style.display= 'none';
+    document.getElementById(`grey${node}`).style.display='block';
+    await sleep(2000);
 
     while(queue.length){
         var current = queue.shift();
 
-        if(!visited[current]){
+        if(!visited[current])
+        {
             console.log(current);
+            document.getElementById(`grey${current}`).style.display='none';
+            document.getElementById(`black${current}`).style.display='block';
+            await sleep(2000);
             visited[current] = true;
 
             for(var i = 0; i < graph.length; i++){
-                if(graph[current][i] && !visited[i]){
+                if(graph[current][i] == !visited[i]){
                     queue.push(i);
+                    document.getElementById(`white${i}`).style.display='none';
+                    document.getElementById(`grey${i}`).style.display='block';
+                    await sleep(2000);
                 }
             }
         }
