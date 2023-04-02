4152337c012585ec3b2cefcd70d0b106deb09e27
LohithCV <lohithcv.211cs136@nitk.edu.in>
2023-04-02 16:55:50 +0530

    Quick Sort UI and Beautification added

--- sorting/quickSort/input.js
+++ sorting/quickSort/input.js
@@ -7,8 +7,17 @@ function Add(){
     // i++;
     // document.getElementById("input").value="";
 
+    // var div = document.createElement("div");
+    // div.setAttribute("class","algo-container");
+    // div.setAttribute("width","1000px !important");
+    // var p = document.createElement("p");
+    // p.setAttribute("id","Display");
+    // div.appendChild(p);
+    // document.body.appendChild(div);
+
     const inputArray = document.getElementById("input").value;
     arr = inputArray.split(",").map(Number);
+    document.getElementById("Display").innerHTML+="The given Array :    ";
     document.getElementById("Display").innerHTML+=arr;
     console.log(arr);
 }
